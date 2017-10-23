7c0fe2f99536706d47cc48da22c3fbc2563f25f0
Raymond Ho <chunkiat82@gmail.com>
2017-10-23 09:29:41 +0800

    Update guard.lua
    Adding Sticky Cookie

--- guard.lua
+++ guard.lua
@@ -1,7 +1,9 @@
 local jwt = require "resty.jwt"
 local jwt_token = ngx.var.arg_jwt
 if jwt_token then
-    ngx.header['Set-Cookie'] = "jwt=" .. jwt_token
+    local expires = 3600 * 24 * 365  -- 365 days
+    local cookie_str = "jwt=" .. jwt_token .. "; Path=/; Expires=" .. ngx.cookie_time(ngx.time() + expires)
+    ngx.header["Set-Cookie"] = cookie_str
 else
     jwt_token = ngx.var.cookie_jwt
 end
@@ -11,7 +13,7 @@ local jwt_obj = jwt:verify("lua-resty-jwt", jwt_token)
 if not jwt_obj["verified"] then
     local site = ngx.var.scheme .. "://" .. ngx.var.http_host;
     local args = ngx.req.get_uri_args();
-
+    -- echo "i am protected by jwt guard";
     ngx.status = ngx.HTTP_UNAUTHORIZED
     ngx.say(jwt_obj.reason);
     ngx.exit(ngx.HTTP_OK)
